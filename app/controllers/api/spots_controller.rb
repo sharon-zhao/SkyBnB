d92dc7498587ecb4d4dca77fa7453068ed2045c6
赵馨睿 <zhaoxinrui@eis-MacBook-Pro.local>
2020-10-31 11:18:50 -0400

    spot rb

---
+++ app/controllers/api/spots_controller.rb
@@ -0,0 +1,42 @@
+class Api::SpotsController < ApplicationController
+  before_action :require_login, only: [:create]
+  def create
+    @spot = Spot.new(spot_param)
+    @spot.host_id = current_user.id
+    if @spot.save
+      render :show
+    
+  end
+
+  def update
+    @spot = current_user.spots.find(param[:id])
+    if @spot.update(spot_param)
+      render :show
+    else
+      render json: @spot.error.full_messages, status: 422
+    end
+  end
+
+  def show
+    @spot = Spot.find(params[:id])
+    render :show
+  end
+
+  def index
+    if params[:title]
+    query_string = "%#{params[:title]}%".downcase
+    @spots = Spot.where("LOWER(title) LIKE ?" , query_string)
+    else
+    @spots = Spot.all
+  end
+  end
+
+  private
+  def spot_param
+    params.require(:spot).permit(:title, :details, :lat, :lng)
+  end
+
+  def bounds
+    params[:bounds]
+  end
+end
